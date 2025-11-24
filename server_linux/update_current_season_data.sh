8ac27bfa47c2b3ba13c3e14583ba71a581e72a75
Ivan Chow <ivanchow23@hotmail.com>
2025-11-23 20:52:19 -0700

    [server] Add script to update current season data
    Script downloads current season and re-generates data files.

---
+++ server_linux/update_current_season_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Download and re-generate data that includes the latest current season
+
+# Configurations
+source configs.sh
+source .env
+
+# Processing
+echo "Updating current season ${CURRENT_SEASON} data..."
+
+# Download latest season data
+echo "Downloading data..."
+../espn_league54078_fantasy_stats/espn_fantasy_api_scripts/espn_fantasy_api_downloader.py -l ${LEAGUE_ID} -s ${CURRENT_SEASON} -e ${CURRENT_SEASON} -o ${ESPN_FANTASY_API_DOWNLOADS_DIR} --espn_s2 "${ESPN_S2}"
+
+# Re-generate data
+./generate_data.sh
