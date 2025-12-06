d17ca0b6766bdc65c144c98ed7f2de2cb0aa510d
Ivan Chow <ivanchow23@hotmail.com>
2025-12-06 14:51:26 -0700

    [streamlit] Removed page 2 and 3 templates

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -2,11 +2,8 @@ import streamlit as st
 
 # Define the pages
 current_season_page = st.Page("current_season_page.py", title="Current Season")
-page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
-page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
-
 # Set up navigation
-pg = st.navigation([current_season_page, page_2, page_3])
+pg = st.navigation([current_season_page])
 
 # Run the selected page
 pg.run()
