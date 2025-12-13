9e5c781ea3f707a6ddbd0b7b0d678a954acb4f0b
Ivan Chow <ivanchow23@hotmail.com>
2025-12-13 13:40:14 -0700

    Remove HTML dashboard generator
    GitHub static HTML page now replaced by streamlit app.

--- server_linux/update_current_season_all.sh
+++ server_linux/update_current_season_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Updates all current season related items
-# Script downloads/re-generates current season data, updates dashboard, pushes to GitHub Pages, etc.
+# Script downloads/re-generates current season data, commits data, pushes to remotes/origin, etc.
 # Intended to be attached to a periodic cron job, or can be ran standalone.
 
 echo "Running update_current_season_all.sh..."
@@ -17,10 +17,7 @@ formatted_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 # Update current season data
 ./update_current_season_data.sh
 
-# Update dashboard
-uv run ../html_dashboard_generator/html_dashboard_generator.py -f ${HTML_DASHBOARD_OUTPUT_DIR}
-
 # Push contents to GitHub Pages
 git add ../docs
-git commit -m "Update dashboard/data ${formatted_datetime}" -m "Auto-generated commit with update_current_season_all.sh."
+git commit -m "Update data ${formatted_datetime}" -m "Auto-generated commit with update_current_season_all.sh."
 git push origin master
