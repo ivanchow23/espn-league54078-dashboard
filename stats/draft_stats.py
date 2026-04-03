4a65bd1c475a8961b163471f591593eaab5a2950
Ivan Chow <ivanchow23@hotmail.com>
2026-04-03 12:16:37 -0600

    [streamlit] Add draft stats to owner profiles
    Added birth countries and player age charts to owner
    profiles page. Copy helper functions from season dashboard
    page. Also removed season selectbox since owner profiles can
    be used to show overall/total stats.

--- stats/draft_stats.py
+++ stats/draft_stats.py
@@ -11,17 +11,23 @@ class DraftStats():
         """ Get number of unique owners in the draft. """
         return sorted(self._draft_df['Owner Name'].unique())
 
-    def get_draft_birth_country_data(self, owner, season):
+    def get_draft_birth_country_data(self, owner, season=None):
         """ Get data for owner's draft birth countries for the given season. """
-        season_df = self._draft_df[self._draft_df['Season'] == season]
-        owner_df = season_df[season_df['Owner Name'] == owner]
+        df = self._draft_df
+        if season is not None:
+            df = df[df['Season'] == season]
+
+        owner_df = df[df['Owner Name'] == owner]
         series = owner_df['Player Birth Country'].value_counts()
         return series
 
-    def get_draft_player_age_data(self, owner, season):
+    def get_draft_player_age_data(self, owner, season=None):
         """ Get data for owner's draft player age for the given season. """
-        season_df = self._draft_df[self._draft_df['Season'] == season]
-        owner_df = season_df[season_df['Owner Name'] == owner]
+        df = self._draft_df
+        if season is not None:
+            df = df[df['Season'] == season]
+
+        owner_df = df[df['Owner Name'] == owner]
         series = owner_df['Player Age'].value_counts()
         return series
 
--- streamlit/owner_profiles_page.py
+++ streamlit/owner_profiles_page.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import plotly.graph_objects as go
 import streamlit as st
 
 # Workaround for import stats to deploy on streamlit app
@@ -9,7 +10,51 @@ import sys
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(SCRIPT_DIR, ".."))
 
+from stats.draft_stats import DraftStats
+
 DRAFT_CSV_PATH = os.path.join(SCRIPT_DIR, "..", "docs", "data", "draft_df.csv")
+
+draft_stats = DraftStats(DRAFT_CSV_PATH)
+
+# ---------------------------------------- Helper Functions ---------------------------------------
+def get_draft_birth_country_fig(owner):
+    """ Helper function to return a plotly figure for draft birth country data. """
+    series = draft_stats.get_draft_birth_country_data(owner)
+
+    wedge_colour_map={"CAN": '#cd5c5c', "USA": '#4169e1', "RUS/USSR": '#fffafa', "SWE": '#ffd700',
+                      "FIN": '#000080', "CZE": '#add8e6', "SVK": '#add8e6', "Czechoslovakia": '#add8e6',
+                      "SUI": '#b22222', "DEN": '#911b1b', "NOR": '#da2a2a', "GER": '#696969'}
+
+    fig = go.Figure()
+    wedge_colours = [wedge_colour_map[index] if index in wedge_colour_map else "darkgray" for index in series.index]
+    fig.add_trace(go.Pie(labels=series.index, values=series.values, marker_colors=wedge_colours,
+                         hole=0.35, pull=[0.075 if i == 0 else 0.03 for i, _ in enumerate(series.values)]))
+    fig.update_layout(title="Player Birth Countries", margin=dict(t=50, b=50), height=400)
+    return fig
+
+def get_draft_age_fig(owner):
+    """ Helper function to return a plotly figure for draft age data. """
+    series = draft_stats.get_draft_player_age_data(owner)
+
+    # Stats
+    try:
+        mean = round(series.index.to_series().mean(), 1)
+    except ValueError:
+        mean = 0
+    try:
+        min = int(series.index.to_series().min())
+    except ValueError:
+        min = 0
+    try:
+        max = int(series.index.to_series().max())
+    except ValueError:
+        max = 0
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=series.index, y=series.values))
+    fig.update_layout(title=f"Player Age", xaxis_title=f"Age (Min = {min} | Mean = {mean} | Max = {max})",
+                      yaxis_title="# of Picks", margin=dict(t=50, b=10), height=400)
+    return fig
 
 # -------------------------------------- Page content start ---------------------------------------
 # Page configs
@@ -23,7 +68,7 @@ draft_df = pd.read_csv(DRAFT_CSV_PATH)
 select_options_container = st.container()
 select_options_cols = select_options_container.columns([1, 1, 1, 1])
 
-# Custom CSS to make season selectbox a sticky header
+# Custom CSS to make selectbox a sticky header
 # Reference: https://discuss.streamlit.io/t/is-it-possible-to-create-a-sticky-header/33145 and Copilot
 select_options_container.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
 select_options_container.markdown(
@@ -45,18 +90,12 @@ select_options_container.markdown(
 
 # Select owners to show
 owners_select_options = sorted(draft_df['Owner Name'].unique())
-select_options_cols[0].markdown("##### Owner")
+select_options_cols[0].markdown("#### Owner")
 selected_owner = select_options_cols[0].selectbox(label="Owner", options=owners_select_options, key="owners", label_visibility='collapsed')
 
-# Select season to show
-seasons = sorted(draft_df['Season'].unique(), reverse=True)
-seasons_select_options = sorted(draft_df['Season'].astype(str).unique(), reverse=True)
-seasons_select_options = [season[:4] + "-" + season[4:] for season in seasons_select_options]
-seasons_select_options[0] = f"{seasons_select_options[0]} (Current)"
-select_options_cols[1].markdown("##### Season")
-selected_season_str = select_options_cols[1].selectbox(label="Season", options=seasons_select_options, key="seasons", label_visibility='collapsed')
-selected_season_str = selected_season_str.replace("-", "")
-if "Current" in selected_season_str:
-    selected_season = seasons[seasons.index(int(selected_season_str.strip(" (Current)")))]
-else:
-    selected_season = seasons[seasons.index(int(selected_season_str))]
+# Draft stats container
+st.markdown("#### Draft Stats")
+container = st.container(border=True, height="stretch", width="stretch", vertical_alignment="center", horizontal_alignment="center")
+container_cols = container.columns([1, 0.25, 1])
+container_cols[0].plotly_chart(get_draft_birth_country_fig(selected_owner))
+container_cols[2].plotly_chart(get_draft_age_fig(selected_owner))
